5942cfb270d547dd413e39e6a7f511d5850efcb1
Anthony Morales <anthony.morales4452@utc.edu.ec>
2025-07-21 12:15:47 -0500

    Plantilla

--- Aplicaciones/Proposito/apps.py
+++ Aplicaciones/Proposito/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PropositoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Proposito'
+    name = 'Aplicaciones.Proposito'
